302afeba6e3702401693a8ceb1d86e0a19b00c1f
Jamila <jamsullivan2307@hotmail.com>
2026-02-11 22:30:27 +0000

    creating a standardised directory structure.

---
+++ directory_creation.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+
+mkdir -p ~/NGS_analysis/{data/{raw,trimmed,reference,metadata},\
+results/{qc,alignment,variants,coverage,annotation},\
+scripts,\
+logs,\
+config,\
+docs}
+
+echo "Directory structure created!"
+
